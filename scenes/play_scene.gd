417e313099c2574250d6d44b4f41ca8aa020e726
Andrew Crisp <andrew.crisp@gmail.com>
2025-04-03 17:03:45 -0600

    Initially grab and drop an item

---
+++ scenes/play_scene.gd
@@ -0,0 +1,6 @@
+extends Node2D
+#
+#func _ready():
+	#print("Setting viewport size")
+	#ProjectSettings.set("display/window/size/viewport_width", 320)
+	#ProjectSettings.set("display/window/size/viewport_height", 240)
---
+++ scripts/sortable_item.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+var isFollowingMouse = false
+var target = Vector2.ZERO
+var speed = 5
+var velocity
+var hoveringArea
+var lastArea
+
+func _ready():
+	pass
+	
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("click"):
+		handleClick()
+			
+	if isFollowingMouse:
+		move()
+		
+func move():
+	position = get_global_mouse_position()
+
+func handleClick():
+	if isFollowingMouse:
+		drop()
+	else:
+		grab()
+
+func drop():
+	isFollowingMouse = false
+	if hoveringArea != null && $Area2D.overlaps_area(hoveringArea):
+		position = hoveringArea.global_position
+		lastArea = hoveringArea
+	elif lastArea != null:
+		position = lastArea.global_position
+		
+	
+func grab():
+	isFollowingMouse = true
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	hoveringArea = area
+
+func _on_area_2d_area_exited(_area: Area2D) -> void:
+	hoveringArea = null
