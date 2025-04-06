1a8e46e1f0656086b69deb43f624bcbeadc320fd
Andrew Crisp <andrew.crisp@gmail.com>
2025-04-05 19:16:35 -0600

    fix on_ready to _ready()

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -6,13 +6,11 @@ func _on_button_pressed() -> void:
 	SpawnNewSortable()
 
 func SpawnNewSortable():
-	if sortable == null:
-		sortable = load("res://scenes/sortable_item.tscn")
 	var newsortable = sortable.instantiate()
 	newsortable.position = global_position
 	newsortable.scale = Vector2(4,4)	
 	get_tree().get_root().get_node("Level").add_child(newsortable)
 
-func _on_ready() -> void:
+func _ready() -> void:
 	sortable = preload("res://scenes/sortable_item.tscn")
 	
