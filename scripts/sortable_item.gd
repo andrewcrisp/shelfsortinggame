34debc695452bfa03a7f65b3c900f25c4e7ebf3c
Andrew Crisp <andrew.crisp@gmail.com>
2025-04-16 19:15:07 -0600

    Do input check only on click

--- scripts/sortable_item.gd
+++ scripts/sortable_item.gd
@@ -5,23 +5,25 @@ var isFollowingMouse = false
 var dragposition = 0
 var lastArea
 var hoveredArea = null
-var mouse_in = false
+#var mouse_in = false
 	
 func _ready() -> void:
 	$"Area2D/CollisionShape2D".shape.size = $Sprite2D.region_rect.size * 1.1
-	$Area2D.connect("mouse_entered", _on_area_2d_mouse_entered)
-	$Area2D.connect("mouse_exited", _on_area_2d_mouse_exited)
+	#$Area2D.connect("mouse_entered", _on_area_2d_mouse_entered)
+	#$Area2D.connect("mouse_exited", _on_area_2d_mouse_exited)
 	$Area2D.connect("area_entered", _on_area_2d_area_entered)
 	$Area2D.connect("area_exited", _on_area_2d_area_exited)
 	
 func _input(event: InputEvent) -> void:
-	if (event is InputEventMouseButton):
+	if (event is InputEventMouseButton
+		|| event is InputEventScreenTouch):
 		if (event.is_pressed()):
-			tryGrab()
-			#isFollowingMouse = true
+			var localPos = to_local(event.position)
+			var boundbox:Shape2D = $Area2D/CollisionShape2D.shape
+			if boundbox.get_rect().has_point(localPos):
+				tryGrab()
 		else:
 			drop()
-			#global_position = event.position
 	if isFollowingMouse:
 		dragposition = event.position
 	
@@ -45,7 +47,7 @@ func drop():
 			lastArea.get_parent().hold_item($".")
 		
 func tryGrab():
-	if Globals.game.currentLevel.carriedItem == null && mouse_in:
+	if Globals.game.currentLevel.carriedItem == null:
 		if !lastArea == null:
 			lastArea.get_parent().remove_item()
 		z_index = Globals.z_levels["held_item"]
@@ -58,11 +60,3 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if (hoveredArea == area):
 		hoveredArea = null
-
-func _on_area_2d_mouse_entered() -> void:
-	mouse_in = true
-	print("in")
-
-func _on_area_2d_mouse_exited() -> void:
-	mouse_in = false
-	print("out")
