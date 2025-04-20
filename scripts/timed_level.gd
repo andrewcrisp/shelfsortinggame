7fc1a464e5778a28fa1f784b5554e9c1da50455a
Andrew Crisp <andrew.crisp@gmail.com>
2025-04-20 15:52:12 -0600

    Make a level timer

---
+++ scripts/timed_level.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const MINUTES: int = 60
+@onready var timer = $Timer
+@onready var timedisplay = $Container/Label
+
+
+func _ready() -> void:
+	timer.wait_time = 2 * MINUTES
+	#timer.start()
+	timedisplay.text = String.num(timer.wait_time, 1)
+	var viewportSize = get_viewport().get_visible_rect().size
+	$Container.size = viewportSize
+	$Container/Label.position.x = viewportSize.x/2
+	
+func _process(_delta: float) -> void:
+	if !(timer.is_stopped()):
+		timedisplay.text = String.num(timer.time_left, 1)
