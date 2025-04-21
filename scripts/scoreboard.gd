2c91ade6e4e893ebb8a606351dfd3dbf72298031
Andrew Crisp <andrew.crisp@gmail.com>
2025-04-20 18:40:03 -0600

    Play with font sizes

--- scripts/scoreboard.gd
+++ scripts/scoreboard.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	score = 0
 	
 func _process(_delta: float) -> void:
-	$RichTextLabel.text = str(score)
+	$ScoreLabel.text = str(score)
 	
 func score_item(points):
 	score += points
--- scripts/timed_level.gd
+++ scripts/timed_level.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 const MINUTES: int = 60
 @onready var timer = $Timer
-@onready var timedisplay = $Container/Label
+@onready var timedisplay = $Label
 
 
 func _ready() -> void:
@@ -11,7 +11,7 @@ func _ready() -> void:
 	timedisplay.text = String.num(timer.wait_time, 1)
 	var viewportSize = get_viewport().get_visible_rect().size
 	$Container.size = viewportSize
-	$Container/Label.position.x = viewportSize.x/2
+	timedisplay.position.x = viewportSize.x/2
 	
 func _process(_delta: float) -> void:
 	if !(timer.is_stopped()):
