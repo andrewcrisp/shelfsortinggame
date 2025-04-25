b149a8ce2eb9d5dd14310576a7607f24c7524dcb
Andrew Crisp <andrew.crisp@gmail.com>
2025-04-24 18:23:56 -0600

    Mock up a high score board

---
+++ scripts/score_entry.gd
@@ -0,0 +1,11 @@
+class_name ScoreEntry extends Control
+
+var gameMode:String = "Game Mode"
+var dateScored:String = "Date"
+var score:String = "Score"
+
+func _ready() -> void:
+	$HBoxContainer/GameModeLabel.text = gameMode
+	
+	$HBoxContainer/DateLabel.text = dateScored
+	$HBoxContainer/Score.text = score	
