dc427210f8883e4fabff77ad93269392f785242d
Andrew Crisp <andrew.crisp@gmail.com>
2025-04-26 19:36:25 -0600

    format date output

--- scripts/score_report.gd
+++ scripts/score_report.gd
@@ -1,4 +1,5 @@
 class_name ScoreReport extends Node2D
+var dateFormat = "%s/%s/%s %s:%s" #4/25/2025 13:00:00
 
 func _ready():
 	var viewportSize = get_viewport().get_visible_rect().size
@@ -9,7 +10,14 @@ func AddScore(score:String, gamemode:String):
 	$Control/ScoreLabel.text = str(score)
 	var newScore:ScoreEntry = load("res://scenes/score_entry.tscn").instantiate()
 	newScore.gameMode = gamemode
-	newScore.dateScored = Time.get_datetime_string_from_system()
+	var dateScored := Time.get_datetime_dict_from_system()
+	newScore.dateScored = dateFormat % [
+		dateScored["month"], 
+		dateScored["day"],
+		dateScored["year"],
+		dateScored["hour"],
+		dateScored["minute"]
+		]
 	newScore.score = score
 	$Control/HighScoreViewer.hiscores.append(newScore)
 	$Control/HighScoreViewer.populateScoresList()
