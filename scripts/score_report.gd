01f35d28c614aee5c1343f33f0e096ecd7605e9e
Andrew Crisp <andrew.crisp@gmail.com>
2025-05-07 19:55:01 -0600

    Position and polish the credits

--- scripts/score_report.gd
+++ scripts/score_report.gd
@@ -2,12 +2,11 @@ class_name ScoreReport extends Node2D
 var dateFormat = "%s/%s/%s %s:%s" #4/25/2025 13:00:00
 
 func _ready():
-	var viewportSize = get_viewport().get_visible_rect().size
-	$Control.size = viewportSize
-	#AddScore("25", "mygame")
+	pass
+
 
 func AddScore(score:String, gamemode:String):
-	$HBoxContainer/ScoreLabel.text = str(score)
+	$Frame/ScoreLabel.text = str(score)
 	var newScore:ScoreEntry = load("res://scenes/components/score_entry.tscn").instantiate()
 	newScore.gameMode = gamemode
 	var dateScored := Time.get_datetime_dict_from_system()
